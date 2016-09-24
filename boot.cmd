ddc678d46dd7666858b8d002881d254f81bf1cb8
Misko <mouse9911@gmail.com>
2016-09-24 16:16:36 -0700

    uboot2016 fixes for booting off SDcard

--- boot.cmd
+++ boot.cmd
@@ -1,4 +1,6 @@
-fatload mmc 0 0x43000000 script.bin
-fatload mmc 0 0x48000000 uImage
-setenv bootargs console=tty0 root=/dev/mmcblk0p2 rootwait panic=10
-bootm 0x48000000
+setenv bootm_boot_mode sec
+setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 rootwait panic=10
+load mmc 0:1 0x43000000 script.bin || load mmc 0:1 0x43000000 boot/script.bin
+load mmc 0:1 0x42000000 uImage || load mmc 0:1 0x42000000 boot/uImage
+setenv machid 10bb
+bootm 0x42000000
